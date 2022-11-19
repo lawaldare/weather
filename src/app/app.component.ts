1350e1f624752f540c7bcc32316107a5afdf7824
Dare Lawal <dare.lawal@originmarkets.com>
2022-11-19 20:46:37 +0000

    refectored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,22 @@
-import { WeatherService } from './services/weather.service';
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { WeatherService } from "./services/weather.service";
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { take } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-
-  weatherReport: any;
+  weatherReport$: any;
   cityName: string;
 
-  constructor(private weather: WeatherService) { }
-
+  constructor(private weather: WeatherService) {}
 
   onSubmit(form: NgForm) {
     this.cityName = form.value.city;
-    this.weather.getWeather(this.cityName).subscribe(data => {
-      this.weatherReport = data;
-      console.log(this.weatherReport);
-    }, error => {
-      console.log(error.message)
-    })
+    this.weatherReport$ = this.weather.getWeather(this.cityName).pipe(take(1));
     form.resetForm();
   }
 }
